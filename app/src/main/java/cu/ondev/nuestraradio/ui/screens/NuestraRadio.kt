82ca34f29e483e610cd4a5d469357fc8b6d9991e
Orlando Novas Rodriguez <38017009+orlando-dev-code@users.noreply.github.com>
2021-09-17 15:20:12 -0700

    Add image loader library

--- app/src/main/java/cu/ondev/nuestraradio/ui/screens/NuestraRadio.kt
+++ app/src/main/java/cu/ondev/nuestraradio/ui/screens/NuestraRadio.kt
@@ -1,15 +1,16 @@
 package cu.ondev.nuestraradio.ui.screens
 
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Card
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -18,21 +19,108 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
+import cu.ondev.nuestraradio.R
 import cu.ondev.nuestraradio.ui.theme.*
 
 
 @Composable
 fun NuestraRadio() {
-    Banner(modifier = Modifier.height(230.dp))
+    Box {
+        Banner(
+            modifier = Modifier
+                .height(300.dp)
+                .padding(bottom = 35.dp)
+        )
+        RadioControls(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+        )
+    }
+}
+
+@Composable
+fun RadioControls(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier
+            .padding(start = 30.dp, top = 8.dp, bottom = 8.dp, end = 30.dp)
+            .height(70.dp)
+    ) {
+        Card(
+            elevation = 8.dp,
+            shape = CircleShape,
+            modifier = modifier
+                .fillMaxSize()
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(8.dp)
+            ) {
+                Image(
+                    painter = rememberImagePainter(R.drawable.radio_test),
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .size(50.dp)
+                        .clip(CircleShape), contentDescription = "Nuestra Radio"
+                )
+                Spacer(modifier = Modifier.width(12.dp))
+                Column(
+                    verticalArrangement = Arrangement.SpaceAround, modifier = Modifier
+                        .fillMaxSize()
+                        .weight(0.3f)
+                ) {
+                    Text(text = "Radio Name")
+                    Text(text = "Visitantes")
+                }
+
+                IconButton(
+                    onClick = {
+
+                    },
+                    modifier = Modifier
+                        .size(40.dp)
+                        .clip(CircleShape)
+                        .background(color = OrangeDark)
+                ) {
+                    Icon(
+                        Icons.Filled.PlayArrow,
+                        tint = Color.White,
+                        contentDescription = "favorites"
+                    )
+                }
+                Spacer(modifier = Modifier.width(12.dp))
+                IconButton(
+                    onClick = {
+
+                    },
+                    modifier = Modifier
+                        .size(40.dp)
+                        .clip(CircleShape)
+                        .background(color = Gray)
+                ) {
+                    Icon(
+                        Icons.Filled.SkipNext,
+                        tint = Color.White,
+                        contentDescription = "favorites"
+                    )
+                }
+            }
+        }
+    }
 }
 
 @Composable
 fun Banner(modifier: Modifier = Modifier) {
 
-    var isFavorite = remember { mutableStateOf(false) }
+    val isFavorite = remember { mutableStateOf(false) }
 
     DrawBackground(
         modifier = modifier
@@ -80,13 +168,13 @@ fun Banner(modifier: Modifier = Modifier) {
                     .background(color = OrangeDark)
             ) {
 
-                if(!isFavorite.value) {
+                if (!isFavorite.value) {
                     Icon(
                         Icons.Filled.FavoriteBorder,
                         tint = Color.White,
                         contentDescription = "favorites"
                     )
-                }else {
+                } else {
                     Icon(
                         Icons.Filled.Favorite,
                         tint = Color.White,
